d37ca413c0f1db0ecb05989f233d652fd62b45ba
garicodes <gariel.lynn@gmail.com>
2023-04-08 00:45:51 -0500

    Added more selectors for email, gender, mobile, DOB, hobbies, and current address

--- pages/form_page.py
+++ pages/form_page.py
@@ -14,3 +14,45 @@ class FormPage:
 
     def input_last_name(self):
         return self.driver.find_element(By.CSS_SELECTOR, "input[id='lastName']")
+
+    def input_user_email(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "input[id='userEmail']")
+
+    def label_male(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "label[for='gender-radio-1']")
+
+    def label_female(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "label[for='gender-radio-2']")
+
+    def label_other(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "label[for='gender-radio-3']")
+
+    def input_date_birth(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "input[id='dateOfBirthInput']")
+
+    def select_january_february_march_april(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "select[class$='month-select']")
+
+    def option_june(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "option[value='5']")
+
+    def select_react_year(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "select[class$='year-select']")
+
+    def option(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "option[value='1946']")
+
+    def div_choose_thursday_june(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "div[class='react-datepicker__day react-datepicker__day--013']")
+
+    def label_custom(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "label[for='hobbies-checkbox-1']")
+
+    def textarea_current_address(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "textarea[id='currentAddress']")
+
+    def input_react_select(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "input[id='react-select-3-input']")
+
+    def div_state(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "div[id='state']")
