03ac45f9b9aa43787527f25303262c6ee5391d44
garicodes <gariel.lynn@gmail.com>
2023-04-07 22:07:07 -0500

    Created utils file that creates way to open browser and close browser

--- utils/selenium_utils.py
+++ utils/selenium_utils.py
@@ -2,10 +2,12 @@ from selenium import webdriver
 
 
 def open_browser():
+    # Creating instance of ChromeDriver
     driver = webdriver.Chrome()
 
     return driver
 
 
 def close_browser(driver):
+    # Setting driver to close
     driver.quit()
